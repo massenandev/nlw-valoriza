fa5e69714b4b2c36f9c32792ac5c0cb7a3c8f013
massenandev <massenandev@gmail.com>
2021-06-24 00:18:10 +0100

    feat(tags): add service, entity, migration, controller and middleware

--- README.md
+++ README.md
@@ -10,11 +10,11 @@
 
 - Cadastro de TAG
 
- [ ] Não é permitido cadstrar mais de uma tag com o mesmo nome
+ [ x ] Não é permitido cadstrar mais de uma tag com o mesmo nome
 
- [ ] Não é permitido cadastrar tag sem nome
+ [ x ] Não é permitido cadastrar tag sem nome
 
- [ ] Não é permitido o cadastro por usuários que não sejam administradores
+ [ x ] Não é permitido o cadastro por usuários que não sejam administradores
 
  - Cadastro de elogios
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors'
 
 import { router } from './routes'
 
@@ -21,6 +22,19 @@ app.use(express.json())
 
 //inserir todas as rotas farão parte do projeto
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction ) => {
+  if(err instanceof Error){
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3000, () => {
   console.log('server is running')
